0cf015cc717e2d94294aeb28d7c723196b28097d
Hedy Pamungkas <hedypamungkas@gmail.com>
2019-11-11 15:43:07 +0700

    create project

---
+++ Moolecole Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Moolecole Project
+//
+//  Created by Nur Irfan Pangestu on 11/11/19.
+//  Copyright © 2019 moolecole.test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
